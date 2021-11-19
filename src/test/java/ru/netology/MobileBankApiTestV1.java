31901cc0cf85c4863e52ce5e659b797f906013e8
Ekaterina Miheeva <karamilllka@gmail.com>
2021-11-19 12:11:01 +0600

    add test demo-3

--- src/test/java/ru/netology/MobileBankApiTestV1.java
+++ src/test/java/ru/netology/MobileBankApiTestV1.java
@@ -17,7 +17,7 @@ public class MobileBankApiTestV1 {
                 .get("/demo/accounts")
                 // Проверки
                 .then()
-                .statusCode(200);
-               // .body(matchesJsonSchemaInClasspath("accounts.schema.json"));
+                .statusCode(200)
+               .body(matchesJsonSchemaInClasspath("accounts.schema.json"));
     }
 }
